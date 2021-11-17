76d8d2c82a4a8bb7693ab671b67c2fa88449e314
Jan Philip <jan.philip.wahle@gmail.com>
2021-11-17 13:24:20 +0100

    Remove unused files #minor

--- nlpland/cli.py
+++ nlpland/cli.py
@@ -6,10 +6,6 @@ from dotenv import load_dotenv
 import nlpland.data.check as check_
 import nlpland.data.dataset as dataset_
 import nlpland.data.filter as filter_
-import nlpland.modules.count as count_
-import nlpland.modules.scatter as scatter_
-import nlpland.modules.semantic as semantic_
-import nlpland.modules.topic_model as topic_
 from nlpland.constants import (
     ABSTRACT_SOURCE_ANTHOLOGY,
     ABSTRACT_SOURCE_RULE,
@@ -95,117 +91,108 @@ def checkpaper(paper_path: str) -> None:
     check_.check_paper_parsing(paper_path)
 
 
-@cli.command()
-def countabstractsanth() -> None:
-    """Print the amount of abstracts in the ACL Anthology XML files."""
-    check_.count_anthology_abstracts()
+# @cli.command()
+# def countabstractsanth() -> None:
+#     """Print the amount of abstracts in the ACL Anthology XML files."""
+#     check_.count_anthology_abstracts()
 
 
-@cli.command()
-@click.argument("k", type=int)
-@click.option("--ngrams", type=str, default="1")
-@filter_.df_filter_options
-def count(k: int, ngrams: str, **kwargs: FILTER_DATATYPES) -> None:
-    """Extract the top k token from a dataframe both in regards to tf and tfidf score.
+# @cli.command()
+# @click.argument("k", type=int)
+# @click.option("--ngrams", type=str, default="1")
+# @filter_.df_filter_options
+# def count(k: int, ngrams: str, **kwargs: FILTER_DATATYPES) -> None:
+#     """Extract the top k token from a dataframe both in regards to tf and tfidf score.
 
-    Args:
-        k: Determines how many tokens we show.
-        ngrams: The lower and upper bound of the n_grams, e.g. "1", "2", "1,2" or "1, 3".
-        **kwargs: Dict of filter to apply to the data before analyzing the data.
-    """
-    # works like filters:
-    # leaving both blank: whole dataset (once)
-    # leaving the second blank: only count first one
-    df_filtered = filter_.get_filtered_df(kwargs)
+#     Args:
+#         k: Determines how many tokens we show.
+#         ngrams: The lower and upper bound of the n_grams, e.g. "1", "2", "1,2" or "1, 3".
+#         **kwargs: Dict of filter to apply to the data before analyzing the data.
+#     """
+#     # works like filters:
+#     # leaving both blank: whole dataset (once)
+#     # leaving the second blank: only count first one
+#     df_filtered = filter_.get_filtered_df(kwargs)
 
-    count_top, tfidf_top = count_.top_k_tokens(k, df_filtered, ngrams)
-    print(f"Most occurring words in selection:\n{count_top}")
-    print(f"Highest tf-idf scores in selection:\n{tfidf_top}")
-
-
-@cli.command()
-@click.argument("k", type=int)
-@click.option("-n", "--name", type=str)
-@click.option("--ngrams", type=str, default="1")
-@click.option("--tfidf", is_flag=True)
-@filter_.df_filter_options
-def counts_time(k: int, ngrams: str, name: str, tfidf: bool, **kwargs: FILTER_DATATYPES) -> None:
-    """Plot the counts of all terms, that were in a top k in at least one year, over time. The time
-    can be selected via the filters.
-
-    Args:
-        k: Determines how many tokens are shown.
-        ngrams: The lower and upper bound of the n_grams, e.g. "1", "2", "1,2" or "1, 3"
-        name: Name of the output image.
-        tfidf: If True, will rank by tf-idf not tf scores.
-        **kwargs: Dict of filter to apply to the data before analyzing the data.
-    """
-    df_filtered = filter_.get_filtered_df(kwargs)
-    count_.counts_over_time(df_filtered, k, ngrams, name, tfidf, kwargs)  # pylint: disable=R0913
+#     count_top, tfidf_top = count_.top_k_tokens(k, df_filtered, ngrams)
+#     print(f"Most occurring words in selection:\n{count_top}")
+#     print(f"Highest tf-idf scores in selection:\n{tfidf_top}")
 
 
-@cli.command()
-@click.option("--fast", is_flag=True)
-@click.option("-n", "--name", type=str)
-@filter_.df_filter_options
-@filter_.df_filter_options2
-def scatter(fast: bool, name: str, **kwargs: FILTER_DATATYPES) -> None:
-    """Plot the word counts using the package scattertext, by comparing two given sets of data.
+# @cli.command()
+# @click.argument("k", type=int)
+# @click.option("-n", "--name", type=str)
+# @click.option("--ngrams", type=str, default="1")
+# @click.option("--tfidf", is_flag=True)
+# @filter_.df_filter_options
+# def counts_time(k: int, ngrams: str, name: str, tfidf: bool, **kwargs: FILTER_DATATYPES) -> None:
+#     """Plot the counts of all terms, that were in a top k in at least one year, over time. The time
+#     can be selected via the filters.
 
-    Highlight which words were more common in which set of data.
-
-    Args:
-        fast: If True, scattertext will be faster, but less accurate, by using a different model.
-        name: Name of the output HTML file.
-        **kwargs: Dict of filter to apply to the data before analyzing the data.
-    """
-    df_y = filter_.get_filtered_df(kwargs)
-    df_x = filter_.get_filtered_df(kwargs, second_df=True)
-
-    scatter_.plot_word_counts(df_y, df_x, fast, name, kwargs)
-
-
-@cli.command()
-@click.argument("topics", type=int)
-@click.option("-n", "--name", type=str)
-@filter_.df_filter_options
-def topic_train(topics: int, name: str, **kwargs: FILTER_DATATYPES) -> None:
-    """Train a topic model and create an interactive visualization using pyLDAvis.
-
-    Args:
-        topics: The amount of topics to train.
-        name: The name of the model and output HTML file.
-        **kwargs: Dict of filter to apply to the data before analyzing the data.
-    """
-    df_filtered = filter_.get_filtered_df(kwargs)
-    topic_.topic(df_filtered, topics, name)
+#     Args:
+#         k: Determines how many tokens are shown.
+#         ngrams: The lower and upper bound of the n_grams, e.g. "1", "2", "1,2" or "1, 3"
+#         name: Name of the output image.
+#         tfidf: If True, will rank by tf-idf not tf scores.
+#         **kwargs: Dict of filter to apply to the data before analyzing the data.
+#     """
+#     df_filtered = filter_.get_filtered_df(kwargs)
+#     count_.counts_over_time(df_filtered, k, ngrams, name, tfidf, kwargs)  # pylint: disable=R0913
 
 
-@cli.command()
-@click.option("--train", is_flag=True)
-@click.option("-n", "--name", type=str)
-@filter_.df_filter_options
-def fasttext(train: bool, name: str, **kwargs: FILTER_DATATYPES) -> None:
-    """Train a semantic model using fastText with the given data, save it and evaluate it.
+# @cli.command()
+# @click.option("--fast", is_flag=True)
+# @click.option("-n", "--name", type=str)
+# @filter_.df_filter_options
+# @filter_.df_filter_options2
+# def scatter(fast: bool, name: str, **kwargs: FILTER_DATATYPES) -> None:
+#     """Plot the word counts using the package scattertext, by comparing two given sets of data.
 
-    Additionally evaluate the model with some tests.
+#     Highlight which words were more common in which set of data.
 
-    Args:
-        train: If True, retrain the model with the given data.
-        name: Name of the model.
-        **kwargs: Dict of filter to apply to the data before analyzing the data.
-    """
-    df_filtered = filter_.get_filtered_df(kwargs)
-    semantic_.semantic(df_filtered, train, name)
+#     Args:
+#         fast: If True, scattertext will be faster, but less accurate, by using a different model.
+#         name: Name of the output HTML file.
+#         **kwargs: Dict of filter to apply to the data before analyzing the data.
+#     """
+#     df_y = filter_.get_filtered_df(kwargs)
+#     df_x = filter_.get_filtered_df(kwargs, second_df=True)
+
+#     scatter_.plot_word_counts(df_y, df_x, fast, name, kwargs)
+
+
+# @cli.command()
+# @click.argument("topics", type=int)
+# @click.option("-n", "--name", type=str)
+# @filter_.df_filter_options
+# def topic_train(topics: int, name: str, **kwargs: FILTER_DATATYPES) -> None:
+#     """Train a topic model and create an interactive visualization using pyLDAvis.
+
+#     Args:
+#         topics: The amount of topics to train.
+#         name: The name of the model and output HTML file.
+#         **kwargs: Dict of filter to apply to the data before analyzing the data.
+#     """
+#     df_filtered = filter_.get_filtered_df(kwargs)
+#     topic_.topic(df_filtered, topics, name)
 
 
 # @cli.command()
 # @click.option("--train", is_flag=True)
+# @click.option("-n", "--name", type=str)
 # @filter_.df_filter_options
-# def umap(**kwargs: FILTER_DATATYPES) -> None:
-#     df_filtered = filter_.get_filtered_df(kwargs)
-#     semantic_.plot(df)
+# def fasttext(train: bool, name: str, **kwargs: FILTER_DATATYPES) -> None:
+#     """Train a semantic model using fastText with the given data, save it and evaluate it.
 
+#     Additionally evaluate the model with some tests.
+
+#     Args:
+#         train: If True, retrain the model with the given data.
+#         name: Name of the model.
+#         **kwargs: Dict of filter to apply to the data before analyzing the data.
+#     """
+#     df_filtered = filter_.get_filtered_df(kwargs)
+#     semantic_.semantic(df_filtered, train, name)
 
 @cli.command()
 def test() -> None:
@@ -221,8 +208,6 @@ def test() -> None:
         "one two, three.\n four-five, se-\nven, open-\nsource, se-\nve.n, ",
     ]
     print(corpus)
-    # import nltk
-    # print(nltk.stem.WordNetLemmatizer().lemmatize("models"))
 
 
 if __name__ == "__main__":
